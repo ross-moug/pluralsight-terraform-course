ab99b492f39ad4b46c12edc4e29c76fbfe137826
Ross Moug <ross.moug@solarwinds.com>
2020-04-27 11:56:57 +0100

    Refactor to use modules

--- base/base.tf
+++ base/base.tf
@@ -1,10 +1,3 @@
-// AWS provider configuration
-provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region = var.aws_region
-}
-
 resource "aws_instance" "PluralsightExampleServer1" {
   ami = "ami-07c8a3a59f2f7305b"
   instance_type = "t2.micro"
---
+++ dev/dev.tf
@@ -0,0 +1,10 @@
+// AWS provider configuration
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region = var.aws_region
+}
+
+module "base" {
+  source = "../base"
+}
